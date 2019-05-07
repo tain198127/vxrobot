d396cbbe24295315b0e3fe25e44b907bd9428d73
DaneBrown <tain198127@163.com>
2019-05-07 14:17:55 +0800

    1. 使用snow，增加情感分析模块

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ from snownlp import SnowNLP
 
 # 需要监控的群
 ChatRoomUserNames = []
-
+# 本机器人的信息
+robotnickname = ''
 import logging
 
 # logging.basicConfig()
@@ -24,7 +25,12 @@ logger.addHandler(ch)
 cf = configparser.ConfigParser()
 cf.read('./conf/application.conf')
 chatroomsNames = cf.get('wechat', 'chatrooms')
-itchat.auto_login(hotReload=True, enableCmdQR=2)
+
+
+def loginCbk():
+    info = itchat.show_mobile_login()
+
+    logger.info(info)
 
 
 def checker(fn):
@@ -37,8 +43,7 @@ def checker(fn):
 
                 return result
             except Exception as e:
-                pass
-                # logger.error(e)
+                logger.error(e)
 
     return run
 
@@ -61,25 +66,47 @@ def ask(msg):
 def sysInfo(msg):
     logger.info(msg)
 
+
 @checker
 def nlpAnalise(msg):
     logger.info(msg)
+    # 表明是在艾特我
+    if msg.ToUserName == msg.User.Self.UserName:
+        if '语义分析' in msg.Content:
+            realContent = msg.Content.split('语义分析')[1:][0]
+            s = SnowNLP(realContent)
+            # 情感指数
+            emocode = s.sentiments
+            emosentence = ''
+            if emocode >= 0.69:
+                emosentence = '偏正能量'
+            elif emocode <= 0.39:
+                emosentence = '偏负能量'
+            else:
+                emosentence = '靠近中性'
 
-    s = SnowNLP(msg.Content)
-    print('群id:{},群:{},userid{}，用户:{},时间:{}, 内容:{}, 情感:{},状态:{}'.format(msg.User.UserName, msg.User.NickName,
-                                                                        msg.ActualUserName,
-                                                                        msg.ActualNickName, msg.CreateTime, msg.Content,
-                                                                        s.sentiments, msg.Status))
+            print('群id:{},群:{},userid{}，用户:{},时间:{}, 内容:{}, 情感:{},状态:{}'.format(msg.User.UserName, msg.User.NickName,
+                                                                                msg.ActualUserName,
+                                                                                msg.ActualNickName, msg.CreateTime,
+                                                                                realContent,
+                                                                                emocode, msg.Status))
+
+            msg.user.send('@{} 情感指数:{}, 语言{}哦'.format(msg.ActualNickName, emocode,emosentence))
+
 
 # 初始化聊天室监控信息
 def initChatRoom(isUpdate):
     global ChatRoomUserNames
     chatrooms = itchat.get_chatrooms(update=isUpdate, contactOnly=True)
+
     for room in chatrooms:
         if room['NickName'] in chatroomsNames:
             ChatRoomUserNames.append(room['UserName'])
             print("群名称:{:<}       唯一识别号:{:>}".format(room['NickName'], room['UserName']))
 
+
+logininfo = itchat.auto_login(hotReload=True, enableCmdQR=2, loginCallback=loginCbk)
+print(logininfo)
 
 initChatRoom(True)
 
